9cdb5a64990105ced04a1fa7ed2757251929f85d
Caue <nncl@live.com>
2021-07-17 12:21:21 -0300

    feat: Setting up project configuration

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+const Header = () => (
+  <div className="md:col-span-1">
+    <div className="px-4 sm:px-0">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">
+        Simple DeepL API integration
+      </h3>
+      <p className="mt-1 text-sm text-gray-600">
+        Translate any text you cant simply by typing and pressing. Simple as that.
+      </p>
+    </div>
+  </div>
+)
+
+export default Header
---
+++ src/pages/home.js
@@ -0,0 +1,66 @@
+import Header from "../components/header";
+
+const Home = () => {
+  function submit(e) {
+    e.preventDefault();
+    // TODO
+  }
+
+  return (
+    <div className="p-10">
+      <div className="mt-10 sm:mt-0">
+        <div>
+          <Header />
+
+          <div className="mt-5">
+            <form noValidate onSubmit={submit}>
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                        Language
+                      </label>
+                      <select
+                        id="country"
+                        name="country"
+                        autoComplete="country"
+                        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      >
+                        <option>English</option>
+                        <option>French</option>
+                        <option>Portuguese</option>
+                      </select>
+                    </div>
+
+                    <div className="col-span-6">
+                      <label htmlFor="english-text" className="block text-sm font-medium text-gray-700">
+                        English text
+                      </label>
+                      <textarea
+                        name="english-text"
+                        id="english-text"
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Translate
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
