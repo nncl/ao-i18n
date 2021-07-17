942be094ce3661016258c502ace305b151fc611c
Caue <nncl@live.com>
2021-07-17 12:31:17 -0300

    feat(pages): Displaying languages within select options

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import Header from "../components/header";
 
+import languages from '../data/languages'
+
 const Home = () => {
   function submit(e) {
     e.preventDefault();
@@ -10,36 +12,42 @@ const Home = () => {
     <div className="p-10">
       <div className="mt-10 sm:mt-0">
         <div>
-          <Header />
+          <Header/>
 
           <div className="mt-5">
-            <form noValidate onSubmit={submit}>
+            <form noValidate onSubmit={ submit }>
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
                     <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                      <label htmlFor="target_lang"
+                             className="block text-sm font-medium text-gray-700">
                         Language
                       </label>
                       <select
-                        id="country"
-                        name="country"
-                        autoComplete="country"
+                        id="target_lang"
+                        name="target_lang"
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       >
-                        <option>English</option>
-                        <option>French</option>
-                        <option>Portuguese</option>
+                        {
+                          languages.map((item, index) => (
+                            <option key={ index }
+                                    value={ item.source_lang }>
+                              { item.title }
+                            </option>
+                          ))
+                        }
                       </select>
                     </div>
 
                     <div className="col-span-6">
-                      <label htmlFor="english-text" className="block text-sm font-medium text-gray-700">
+                      <label htmlFor="text"
+                             className="block text-sm font-medium text-gray-700">
                         English text
                       </label>
                       <textarea
-                        name="english-text"
-                        id="english-text"
+                        name="text"
+                        id="text"
                         className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         required
                       />
