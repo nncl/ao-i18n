4b04d64f383761efb6970bf6d6aba87f8dc6b0c7
Caue <nncl@live.com>
2021-07-17 12:31:02 -0300

    feat(data): Creating languages' array

---
+++ src/data/languages.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'English',
+    source_lang: 'EN'
+  },
+  {
+    title: 'French',
+    source_lang: 'FR'
+  },
+  {
+    title: 'German',
+    source_lang: 'DE'
+  },
+  {
+    title: 'Portuguese',
+    source_lang: 'PT'
+  },
+]
