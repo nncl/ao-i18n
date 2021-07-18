243a60d8f222029997134f74f81d183fcbb51f64
Caue <nncl@live.com>
2021-07-18 14:00:35 -0300

    fix(components): Fix typo

--- src/components/form.js
+++ src/components/form.js
@@ -97,7 +97,7 @@ const Form = () => {
               className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               disabled={ loading }
             >
-              { loading ? 'Transling...' : 'Translate' }
+              { loading ? 'Translating...' : 'Translate' }
             </button>
           </div>
         </div>
